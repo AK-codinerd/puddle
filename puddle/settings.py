abec19ebe773bb0b47c3378d3262916fda8469a8
Ak-codinerd <mohdakramali545@gmail.com>
2024-03-04 23:12:24 +0530

    debugging

--- puddle/settings.py
+++ puddle/settings.py
@@ -131,8 +131,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR/'media'  # BASE is already mentioned above by default
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'  # BASE is already mentioned above by default
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
